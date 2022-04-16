607e69030ec6c61b0c0470b2157e49cf47a0e9e0
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-16 14:47:50 +0000

    Minor Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,22 @@
 import 'package:flutter/material.dart';
 
-void main(){}
+void main(){
+runApp(MaterialApp(
+  home: Home(),
+)
+);
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
